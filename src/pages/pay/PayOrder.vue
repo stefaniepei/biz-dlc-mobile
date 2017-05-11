efd48d51803a52dd4c742df93fe1d7e6d91a8223
裴俊柯 <stefaniepei@peijunkedeMacBook-Pro-2.local>
2017-05-11 17:14:53 +0800

    paySuccess

--- src/pages/pay/PayOrder.vue
+++ src/pages/pay/PayOrder.vue
@@ -1,89 +1,98 @@
 <template>
     <div class="page pay-order">
-        <mt-header title="订单支付" class="header-bg-color" fixed>
-            <a @click="this.$router.go(-1)" slot="left">
-                <mt-button icon="back"></mt-button>
-            </a>
-        </mt-header>
-        <section class="order-info header-margin">
-            <div class="w50 inline-block float-left height-4 line-height-4">
-                <span class="margin-left10">产品名称：</span>
-            </div>
-            <div class="w50 inline-block float-left text-right height-4 line-height-4">
-                <span class="margin-right10">{{order.productName}}</span>
-            </div>
+        <div v-show="orderShow">
+            <mt-header title="订单支付" class="header-bg-color" fixed>
+                <a @click="this.$router.go(-1)" slot="left">
+                    <mt-button icon="back"></mt-button>
+                </a>
+            </mt-header>
+            <section class="order-info header-margin">
+                <div class="w50 inline-block float-left height-4 line-height-4">
+                    <span class="margin-left10">产品名称：</span>
+                </div>
+                <div class="w50 inline-block float-left text-right height-4 line-height-4">
+                    <span class="margin-right10">{{order.productName}}</span>
+                </div>
     
-            <div class="w50 inline-block float-left height-4 line-height-4 v-line">
-                <p class="margin-left10 height-2 line-height-2">
-                    <span class="expected" id="annualYield">{{order.productRate}}</span>
-                    <span class="expected" id="annualYieldJX">{{jxBite}}</span>
-                </p>
-                <p class="margin-left10 height-2 line-height-2">
-                    <span class="expected-desc">预期年化收益</span>
-                </p>
-            </div>
-            <div class="w50 inline-block float-left text-right height-4 line-height-4">
-                <p class="margin-right10 height-2 line-height-2">
-                    <span class="expected" id="anticipatedIncome">{{order.expectedProfit}}</span>
-                    <span class="expected" id="anticipatedIncomeJX">{{jxMoney}}</span>
-                </p>
-                <p class="margin-right10 height-2 line-height-2">
-                    <span class="expected-desc">预计收益(元)</span>
-                </p>
-            </div>
-        </section>
-        <section class="fill-div-05"></section>
+                <div class="w50 inline-block float-left height-4 line-height-4 v-line">
+                    <p class="margin-left10 height-2 line-height-2">
+                        <span class="expected" id="annualYield">{{order.productRate}}</span>
+                        <span class="expected" id="annualYieldJX">{{jxBite}}</span>
+                    </p>
+                    <p class="margin-left10 height-2 line-height-2">
+                        <span class="expected-desc">预期年化收益</span>
+                    </p>
+                </div>
+                <div class="w50 inline-block float-left text-right height-4 line-height-4">
+                    <p class="margin-right10 height-2 line-height-2">
+                        <span class="expected" id="anticipatedIncome">{{order.expectedProfit}}</span>
+                        <span class="expected" id="anticipatedIncomeJX">{{jxMoney}}</span>
+                    </p>
+                    <p class="margin-right10 height-2 line-height-2">
+                        <span class="expected-desc">预计收益(元)</span>
+                    </p>
+                </div>
+            </section>
+            <section class="fill-div-05"></section>
     
-        <section class="bgc-fff height-4 line-height-4">
-            <div class="w50 inline-block float-left height-4 line-height-4">
-                <span class="margin-left10">优惠券</span>
-                <span class="red-bag-wenhao" id="showCouponRule"></span>
-            </div>
-            <div class="w50 inline-block float-left text-right height-4 line-height-4">
-                <span>{{couponTxt}}</span>
-                <span class="arrow-right"></span>
-            </div>
-        </section>
-        <section class="fill-div-05"></section>
+            <section class="bgc-fff height-4 line-height-4">
+                <div class="w50 inline-block float-left height-4 line-height-4">
+                    <span class="margin-left10">优惠券</span>
+                    <span class="red-bag-wenhao" id="showCouponRule"></span>
+                </div>
+                <div class="w50 inline-block float-left text-right height-4 line-height-4">
+                    <span>{{couponTxt}}</span>
+                    <span class="arrow-right"></span>
+                </div>
+            </section>
+            <section class="fill-div-05"></section>
     
-        <section class="bgc-fff height-4 line-height-4">
-            <div class="w50 inline-block float-left height-4 line-height-4">
-                <span class="margin-left10">订单金额</span>
-            </div>
-            <div class="w50 inline-block float-left text-right height-4 line-height-4">
-                <span class="margin-right10" style="color:#346faf">{{order.total}}</span>
-            </div>
-        </section>
-        <section class="fill-div-05"></section>
+            <section class="bgc-fff height-4 line-height-4">
+                <div class="w50 inline-block float-left height-4 line-height-4">
+                    <span class="margin-left10">订单金额</span>
+                </div>
+                <div class="w50 inline-block float-left text-right height-4 line-height-4">
+                    <span class="margin-right10" style="color:#346faf">{{order.total}}</span>
+                </div>
+            </section>
+            <section class="fill-div-05"></section>
     
-        <section class="fixed bottom w100 bgc-fff">
-            <section class="pay-xys-2">
-                <section class="reg-agree">
-                    <input type="checkbox" style="display:none" id="chkContract" checked="checked" />
-                    <span class="checkboxIcon checked" id="zfxy"></span>
-                    <span>已阅读并同意
-                        <a href="javascript:;" id="dqsqs">《点理财平台委托代扣授权书》</a>
-                    </span>
+            <section class="fixed bottom w100 bgc-fff">
+                <section class="pay-xys-2">
+                    <section class="reg-agree">
+                        <input type="checkbox" style="display:none" id="chkContract" checked="checked" />
+                        <span class="checkboxIcon checked" id="zfxy"></span>
+                        <span>已阅读并同意
+                            <a href="javascript:;" id="dqsqs">《点理财平台委托代扣授权书》</a>
+                        </span>
+                    </section>
+                </section>
+                <section>
+                    <div class="pay-btn-left">
+                        <span style="color:#333;font-size:1.4rem;">应付总额：</span>
+                        <span style="color:#ff0036;font-size:1.4rem">￥{{balancePamount}}</span>
+                    </div>
+                    <button type="button" id="pay" @click="payOrderTrade" :disabled="btnDisabled" v-on:btnState="toDisabled" class="pay-btn pay-timer">{{buyBtnVal}}</button>
                 </section>
             </section>
-            <section>
-                <div class="pay-btn-left">
-                    <span style="color:#333;font-size:1.4rem;">应付总额：</span>
-                    <span style="color:#ff0036;font-size:1.4rem">￥{{balancePamount}}</span>
-                </div>
-                <button type="button" id="pay" @click="payOrderTrade" :disabled="btnDisabled" v-on:btnState="toDisabled" class="pay-btn pay-timer">{{buyBtnVal}}</button>
-            </section>
-        </section>
+        </div>
+        <pay-success v-show="!orderShow"></pay-success>
     </div>
 </template>
 <script>
+import Vue from 'vue'
 import { mapGetters } from 'vuex'
 import bcrypt from 'bcryptjs'
 import md5 from 'md5'
-import { Toast, MessageBox } from 'mint-ui'
+import { Toast, MessageBox, Popup } from 'mint-ui'
+import paySuccess from 'components/pay-success.vue'
+
+Vue.component(Popup.name, Popup)
+
 export default {
     data() {
         return {
+            orderShow: true,
             order: [{}],
             btnDisabled: 'disabled',
             buyBtnVal: '立即支付',
@@ -112,6 +121,9 @@ export default {
                 vm.balance = vm.userAccount.balance.available
             }
         })
+    },
+    components: {
+        paySuccess
     },
     methods: {
         getOrderDetail(order) {
@@ -181,6 +193,10 @@ export default {
         },
         payOrderTrade() {
             let _this = this
+            this.orderShow = false
+
+
+            return
             MessageBox.prompt(' ', '请输入交易密码', { inputPlaceholder: '请输入交易密码' })
                 .then(({ value, action }) => {
                     console.log(value, action)
